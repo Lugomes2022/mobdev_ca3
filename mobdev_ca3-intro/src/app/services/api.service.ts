e420e76e9c41b9024d97d555859ec26fc724ec7d
Lugomes2022 <lucasgms08@gmail.com>
2022-11-11 16:08:49 +0000

    creating the first service for Characters

---
+++ mobdev_ca3-intro/src/app/services/api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) {}
+
+    getCharacters(){
+      return this.http.get('https://www.breakingbadapi.com/api/characters');
+    }
+    
+
+    getCharacter(id){
+      return this.http.get(`https://www.breakingbadapi.com/api/characters/${id}`);
+    }
+
+
+   
+}
