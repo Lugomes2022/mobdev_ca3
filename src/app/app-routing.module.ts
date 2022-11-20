70fba2c9eb1988c4e69394685e03f98846c2a826
Lugomes2022 <lucasgms08@gmail.com>
2022-11-20 14:22:55 +0000

    excluding files and updating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,31 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'characters',
-    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
-  },
-  {
-    path: 'episodes',
-    loadChildren: () => import('./pages/episodes/episodes.module').then( m => m.EpisodesPageModule)
-  },
-  {
-    path: 'quotes',
-    loadChildren: () => import('./pages/quotes/quotes.module').then( m => m.QuotesPageModule)
-  },
-  {
-    path: 'death-count',
-    loadChildren: () => import('./pages/death-count/death-count.module').then( m => m.DeathCountPageModule)
-  },
+  { path: '', loadChildren: () => import('./pages/tab/tab.module').then( m => m.TabPageModule)},
   {
     path: 'characters-details',
     loadChildren: () => import('./pages/characters-details/characters-details.module').then( m => m.CharactersDetailsPageModule)
@@ -34,7 +10,9 @@ const routes: Routes = [
   {
     path: 'episodes-details',
     loadChildren: () => import('./pages/episodes-details/episodes-details.module').then( m => m.EpisodesDetailsPageModule)
-  },
+  }
+
+ 
 ];
 
 @NgModule({
